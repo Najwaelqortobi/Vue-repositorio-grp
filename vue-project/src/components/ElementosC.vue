dd50e546a7c5206196b983f9788a0d21aecaca18
moisescolman <mosscolman64@gmail.com>
2025-02-12 12:44:18 +0100

    Parentchild +

--- vue-project/src/components/ElementosC.vue
+++ vue-project/src/components/ElementosC.vue
@@ -1,10 +1,10 @@
-<script setup>
-import { usarContador } from "@/composables/usarContador";
-
-const { contador, incrementar } = usarContador();
-</script>
-
 <template>
-  <p>Contador: {{ contador }}</p>
-  <button @click="incrementar">incrementar</button>
+  <div>
+    <h1>Lista de usuarios</h1>
+    <ul>
+      <li><RouterLink to="/user/1">Perfil de Usuario 1</RouterLink></li>
+      <li><RouterLink to="/user/2">Perfil de Usuario 2</RouterLink></li>
+    </ul>
+  </div>
 </template>
+
---
+++ vue-project/src/components/ParentC.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import TarjetaUsuario from "@/components/TarjetaUsuario.vue"
+</script>
+<template>
+    <div>
+        <h3>Usuarios</h3>
+        <TarjetaUsuario nombre="Juan Gilberto" edad="45" />
+        <TarjetaUsuario nombre="Anastacio Quebrado" edad="28" />
+        <TarjetaUsuario nombre="Melchor Melano" edad="36" />
+    </div>
+
+</template>
