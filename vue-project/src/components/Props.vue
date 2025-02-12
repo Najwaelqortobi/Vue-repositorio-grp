92ef3cb52dd5b2ebd59e7f05005148650cbed0d8
Youssef <sidewalking29@mail.com>
2025-02-12 12:36:33 +0100

    Props

---
+++ vue-project/src/components/Props.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps(['message']); // Declare the 'message' prop that the Parent passed
+</script>
+
+<!-- ChildComponent.vue -->
+<template>
+  <!-- Displaying the 'message' prop sent from Parent -->
+  <p>Message from Parent: {{ message }}</p>
+</template>
