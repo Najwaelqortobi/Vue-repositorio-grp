d7ebc4386fcaed1baa814ebc85dff8d0d0a77062
Youssef <sidewalking29@mail.com>
2025-02-17 10:47:58 +0100

    Watch Effect

---
+++ vue-project/src/components/watchEffect.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, watchEffect } from 'vue';
+
+// Variable reactiva
+const count = ref(0);
+
+// watchEffect se ejecuta automáticamente cada vez que count cambia
+watchEffect(() => {
+  console.log(`El valor de count ahora es: ${count.value}`);
+});
+</script>
+
+<template>
+  <div>
+    <p>Count: {{ count }}</p>
+    <button @click="count++">Incrementar</button>
+  </div>
+</template>
